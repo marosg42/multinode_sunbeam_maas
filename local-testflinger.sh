f6c8802db47fa848749f8033bd572e338e94ed6c
Marian Gasparovic <marian.gasparovic@canonical.com>
2025-10-22 13:45:58 +0200

    Adapt to new structure

--- local-testflinger.sh
+++ local-testflinger.sh
@@ -2,12 +2,12 @@
 
 TMP_DIR=$(mktemp -d)
 trap "rm -rf $TMP_DIR; echo 'Cleaned up temporary file.'" EXIT
-cp -rf ../snap-openstack/ $TMP_DIR/repository
+cp -rf . $TMP_DIR/repository
 pushd $TMP_DIR
 tar --exclude=repository/.tox --exclude=repository/.github/workflows/testflinger/repository.tar.gz --exclude=repository/.git -acf   repository.tar.gz repository/
 ls -lh repository.tar.gz
 popd
-export TESTFLINGER_DIR=$(pwd)/.github/workflows/testflinger/
+export TESTFLINGER_DIR=$(pwd)/testflinger/
 cp $TMP_DIR/repository.tar.gz $TESTFLINGER_DIR
 export OPENSTACK_SNAP_PATH=$(ls openstack_*.snap)
 JOB_FILE=$TESTFLINGER_DIR/job.yaml
