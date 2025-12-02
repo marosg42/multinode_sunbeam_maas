879ea70e8c5d503c206995976f208a1e2b774170
Marian Gasparovic <marian.gasparovic@canonical.com>
2025-12-02 14:03:05 +0100

    feat: add juju channel configuration to test script
    Allow specifying the Juju channel via TEST_JUJU_CHANNEL environment variable, defaulting to 4.0/stable for testing purposes.

--- runit.sh
+++ runit.sh
@@ -1,2 +1,2 @@
-TEST_MAAS_API_KEY="$(cat ~/api.key)" TEST_MAAS_URL=http://172.16.1.2:5240/MAAS ./test-juju4.sh
+TEST_JUJU_CHANNEL=4.0/stable TEST_MAAS_API_KEY="$(cat ~/api.key)" TEST_MAAS_URL=http://172.16.1.2:5240/MAAS ./test-juju4.sh
 
